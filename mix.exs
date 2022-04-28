a3cb60460b9736df71858c93acb2b475e42b44d1
Emilano <azadvararvand@gmail.com>
2022-04-28 12:44:48 +0430

    Add files via upload

--- mix.exs
+++ mix.exs
@@ -25,8 +25,8 @@ defmodule Mazaryn.MixProject do
   end
 
   # Specifies which paths to compile per environment.
-  defp elixirc_paths(:test), do: ["lib", "test/support"]
-  defp elixirc_paths(_), do: ["lib"]
+  defp elixirc_paths(:test), do: ["lib", "src", "test/support"]
+  defp elixirc_paths(_), do: ["lib", "src"]
 
   # Specifies your project dependencies.
   #
