6b513f6e7166b0d042f5b7cc5619fcbd4a6048d8
Emilano <azadvararvand@gmail.com>
2022-04-30 13:20:58 +0430

    Add files via upload

--- lib/mazaryn_web/controllers/user_registration_controller.ex
+++ lib/mazaryn_web/controllers/user_registration_controller.ex
@@ -2,4 +2,13 @@ defmodule MazarynWeb.UserRegistrationController do
   use MazarynWeb, :controller
 
   alias Core.UserClient
+
+  def show(conn, %{"username" => username}) do
+    case UserClient.get_user!(username) do
+      {:error, msg} ->
+        render(conn, "error.json", message: msg)
+      {:ok, user} ->
+        render(conn, "show.json", user: user)
+    end
+  end
 end
