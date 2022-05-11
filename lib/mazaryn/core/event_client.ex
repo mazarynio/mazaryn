ad69e4a6d01640ddae12beb95905d8316f4e2021
Emilano <azadvararvand@gmail.com>
2022-05-11 21:20:39 +0430

    Add files via upload

---
+++ lib/mazaryn/core/event_client.ex
@@ -0,0 +1,10 @@
+defmodule Core.EventClient do
+  def start do
+    :event_server.start_link()
+    :event_server.init([])
+  end
+
+  def new_event(name, date, loc, desc) do
+    :event_server.add_event(Name, Date, Loc, Desc)
+  end
+end
---
+++ lib/mazaryn/core/user_client.ex
@@ -0,0 +1,26 @@
+defmodule Core.UserClient do
+  def start do
+    :user_server.start_link()
+    :user_server.init([])
+  end
+
+  def register(username, password) do
+    :user_server.create_account(Username, Password)
+  end
+
+  def getting_user(username) do
+    :user_server.get_user(Username)
+  end
+
+  def getting_users() do
+    :user_server.get_users()
+  end
+
+  def follow_user(username) do
+    :user_server.follow(Username)
+  end
+
+  def creating_post(content) do
+    :user_server.create_post(Content)
+  end
+end
