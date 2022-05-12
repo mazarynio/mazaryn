ce46cbc52b8cd684b5b73e7e90cb86e3490a7041
htdat148 <tandathuynh148@gmail.com>
2022-05-12 15:04:27 +0700

    Add event and user child processes

--- src/otpcode/src/otpcode_sup.erl
+++ src/otpcode/src/otpcode_sup.erl
@@ -16,20 +16,26 @@
 start_link() ->
     supervisor:start_link({local, ?SERVER}, ?MODULE, []).
 
-%% sup_flags() = #{strategy => strategy(),         % optional
-%%                 intensity => non_neg_integer(), % optional
-%%                 period => pos_integer()}        % optional
-%% child_spec() = #{id => child_id(),       % mandatory
-%%                  start => mfargs(),      % mandatory
-%%                  restart => restart(),   % optional
-%%                  shutdown => shutdown(), % optional
-%%                  type => worker(),       % optional
-%%                  modules => modules()}   % optional
+
 init([]) ->
-    SupFlags = #{strategy => one_for_all,
-                 intensity => 0,
+    SupFlags = #{strategy => one_for_one,
+                 intensity => 50,
                  period => 1},
-    ChildSpecs = [],
+    ChildSpecs = [
+                  #{id => event_server,
+                    start => {event_server, start_link, []},
+                    restart => permanent,
+                    shutdown => 5000,
+                    type => worker,
+                    modules => [event_server]},
+
+                  #{id => user_server,
+                    start => {user_server, start_link, []},
+                    restart => permanent,
+                    shutdown => 5000,
+                    type => worker,
+                    modules => [user_server]}
+                  ],
     {ok, {SupFlags, ChildSpecs}}.
 
 %% internal functions
