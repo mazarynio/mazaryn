a583048c5fb6b5686781caea179af7ad439e0fdd
Emilano <azadvararvand@gmail.com>
2022-05-13 11:00:04 +0430

    Add files via upload

---
+++ src/otpcode/src/include/records.hrl
@@ -0,0 +1,8 @@
+-record(user, {id, username, password, email, pid, posts = 0, loc}).
+-record(post, {id, content, comments = []}).
+-record(comment, {username, content}).
+-record(group, {id, gp_name, num_members, members}).
+-record(event, {name, date, loc, desc}).
+-record(msg, {sender, receiver, content, timestamp}).
+-record(follower, {id, username}).
+-record(following, {id, username}). 
