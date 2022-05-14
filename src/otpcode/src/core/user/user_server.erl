abf50778613586f242f0106638210cf7515c321d
Emilano <azadvararvand@gmail.com>
2022-05-14 19:56:25 +0430

    Add files via upload

--- src/otpcode/src/core/user/user_server.erl
+++ src/otpcode/src/core/user/user_server.erl
@@ -1,9 +1,16 @@
 -module(user_server).
--compile([export_all, nowarn_export_all]).
 -behaviour(gen_server).
 -define(SERVER, ?MODULE).
 -record(state, {}).
 -import(postdb, [init/0]).
+
+-export([start_link/0, create_account/3, login/2, set_user_info/3, get_user/1,
+         get_users/0, get_password/1, get_user_by_email/1, change_password/3,
+         change_email/3, change_username/3, delete_user/1, follow/2, unfollow/2,
+         follow_multiple/2, unfollow_multiple/2, save_post/2, unsave_post/2, 
+         save_posts/2, unsave_posts/2, get_save_posts/1, get_following/1, 
+         get_follower/1, get_user_info/1, init/1, handle_call/3, handle_cast/2, 
+         handle_info/2, terminate/2, code_change/3, is_username_taken/1]).
 
 start_link() ->
     gen_server:start_link({global, ?SERVER}, ?MODULE, [], []).  
