637182aa3f17f2e2f51569e8eab6180dc91b38b8
Danilo Moura <danilolmoura@gmail.com>
2022-05-22 05:18:26 -0300

    login: add login handler

--- lib/mazaryn/account/users.ex
+++ lib/mazaryn/account/users.ex
@@ -7,14 +7,31 @@ defmodule Account.Users do
   alias Mazaryn.Mailer
   require Logger
 
+
   def register(username, pass, email) do
+    IO.inspect(UserClient.register(username, pass, email), label: 4343434)
+
     case UserClient.register(username, pass, email) do
-      {:atomic, :ok} ->
+      :ok ->
         username
         |> Mail.UserEmail.register_email(email)
         |> Mailer.deliver()
       res ->
         Logger.error("[Users] Failed to register user #{username}: #{res}")
+        {:error, res}
     end
+  end
+
+  def login(username, pass) do
+    case UserClient.login(username, pass) do
+      :logged_in -> autheticate_user(username)
+      res ->
+       Logger.error("[Users] Failed to login #{username}: #{res}")
+       {:error, res}
+    end
+  end
+
+  defp autheticate_user(_username) do
+    nil
   end
 end
