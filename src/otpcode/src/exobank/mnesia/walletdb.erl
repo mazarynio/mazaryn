54fc1a8c95fbe9eabb82f88456443c818c4a2e19
Emilano <azadvararvand@gmail.com>
2022-05-22 16:02:11 +0430

    Add files via upload

--- src/otpcode/src/exobank/mnesia/walletdb.erl
+++ src/otpcode/src/exobank/mnesia/walletdb.erl
@@ -1,14 +1,21 @@
 -module(walletdb).
--export([insert/1]).
+-export([init/0, insert/2]).
 
--record(wallet, {name, address, balance, pub_key, priv_key}).
+-record(wallet, {name, password, address, balance, pub_key, priv_key}).
 
-insert(Name) ->
+init() ->
+    mnesia:create_schema([node()]),
+    mnesia:start(),
+    mnesia:create_table(wallet, 
+                    [{attributes, record_info(fields, wallet)}]).
+
+insert(Name, Password) ->
     Address = crypto_utils:generate_key_pair(),
     Balance = 0,
     Pub_key = uuid:generate(),
     Priv_key = uuid:get_rand_string(),
-    Wallet = #wallet{name = Name, 
+    Wallet = #wallet{name = Name,
+                     password = Password, 
                      address = Address,
                      balance = Balance,
                      pub_key = Pub_key,
@@ -17,6 +24,8 @@ insert(Name) ->
         mnesia:write(Wallet)
     end,
     mnesia:transaction(F).
+
+
 
 
 
