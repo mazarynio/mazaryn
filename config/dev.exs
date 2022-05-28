a070e5dd75ecf87ab509c330b211a2054b00072f
Emilano <azadvararvand@gmail.com>
2022-05-28 11:26:41 +0430

    Add files via upload

--- config/dev.exs
+++ config/dev.exs
@@ -76,3 +76,6 @@ config :phoenix, :stacktrace_depth, 20
 
 # Initialize plugs at runtime for faster development compilation
 config :phoenix, :plug_init_mode, :runtime
+
+
+config :joken, default_signer: "secret"
