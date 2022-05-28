685265f925811abcaef902eb7142cd7b3da6e19f
Emilano <azadvararvand@gmail.com>
2022-05-28 11:32:12 +0430

    Add files via upload

--- lib/mazaryn/account/user.ex
+++ lib/mazaryn/account/user.ex
@@ -12,4 +12,8 @@ defmodule Account.User do
     other_info: [],
     date_created: nil,
     date_updated: nil
+
+  def new({:user, username, password, email, following, follower, saved_posts, other_info, date_created, date_updated}) do
+    struct(Account.User, %{username: username, password: password, email: email, follower: follower, following: following, saved_posts: saved_posts, other_info: other_info, date_created: date_created, date_updated: date_updated})
+  end
 end
