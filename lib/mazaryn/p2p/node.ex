675dc1092b4b3f79f89b071997bcc1269f59af23
Emilano <azadvararvand@gmail.com>
2022-06-14 20:59:08 +0430

    Add files via upload

---
+++ lib/mazaryn/p2p/node.ex
@@ -0,0 +1,28 @@
+defmodule Mazaryn.P2P.Node do
+  @enforce_keys [
+    :id,
+    :host,
+    :port
+  ]
+
+  defstruct [
+    id: nil,
+    host: nil,
+    port: nil
+  ]
+
+  def new(id, host, port) do
+    %__MODULE__{
+      id: id,
+      host: host,
+      port: port
+    }
+  end
+
+  @doc """
+  Generate twenty-byte node ID.
+  """
+  def generate_node_id do
+    :crypto.strong_rand_bytes(20)
+  end
+end
