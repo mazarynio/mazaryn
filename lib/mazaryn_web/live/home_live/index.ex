1a5e5a8e6b7d3586e44ab07c2de44eb7811df29b
pau <jethro.riosa@gmail.com>
2022-06-15 16:55:46 +0800

    remove js codes

--- lib/mazaryn_web/live/home_live/index.ex
+++ lib/mazaryn_web/live/home_live/index.ex
@@ -1,7 +1,7 @@
 defmodule MazarynWeb.HomeLive.Index do
   use MazarynWeb, :live_view
 
-  def mount(_params, session, socket) do
+  def mount(_params, _session, socket) do
     {:ok, socket}
   end
 end
