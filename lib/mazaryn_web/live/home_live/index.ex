eed8b016f6b8d01ea544e432208108981ff8cf2c
Emilano <azadvararvand@gmail.com>
2022-06-15 10:43:22 +0430

    Add files via upload

--- lib/mazaryn_web/live/home_live/index.ex
+++ lib/mazaryn_web/live/home_live/index.ex
@@ -2,14 +2,6 @@ defmodule MazarynWeb.HomeLive.Index do
   use MazarynWeb, :live_view
 
   def mount(_params, session, socket) do
-
-    IO.inspect(session, label: "session")
     {:ok, socket}
-  end
-
-  def render(assigns) do
-    ~H"""
-      home
-    """
   end
 end
