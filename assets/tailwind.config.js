5071888f2943e52323377bceef30af182ededa89
pau <jethro.riosa@gmail.com>
2022-06-18 02:15:18 +0800

    homepage-body: able to plot post

--- assets/tailwind.config.js
+++ assets/tailwind.config.js
@@ -1,15 +1,12 @@
 // See the Tailwind configuration guide for advanced usage
 // https://tailwindcss.com/docs/configuration
 module.exports = {
-  content: [
-    './js/**/*.js',
-    '../lib/*_web.ex',
-    '../lib/*_web/**/*.*ex'
-  ],
+  content: ["./js/**/*.js", "../lib/*_web.ex", "../lib/*_web/**/*.*ex"],
   theme: {
     extend: {},
+    fontFamily: {
+      sans: ["Poppins", "sans-serif"],
+    },
   },
-  plugins: [
-    require('@tailwindcss/forms')
-  ]
-}
+  plugins: [require("@tailwindcss/forms")],
+};
