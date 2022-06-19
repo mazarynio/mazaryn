a3fab7cf0df4ef4f8832ecbadc11de857fc45593
Emilano <azadvararvand@gmail.com>
2022-06-19 18:15:02 +0430

    Add files via upload

--- assets/tailwind.config.js
+++ assets/tailwind.config.js
@@ -9,4 +9,8 @@ module.exports = {
     },
   },
   plugins: [require("@tailwindcss/forms", "daisyui")],
+
+  daisyui: {
+    themes: ["light", "dark", "cupcake", "bumblebee", "emerald", "corporate", "synthwave", "retro", "cyberpunk", "valentine", "halloween", "garden", "forest", "aqua", "lofi", "pastel", "fantasy", "wireframe", "black", "luxury", "dracula", "cmyk", "autumn", "business", "acid", "lemonade", "night", "coffee", "winter"],
+  },
 };
