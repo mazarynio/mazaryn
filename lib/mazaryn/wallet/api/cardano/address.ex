e63584be4042ce398de8ad8d15966b16cb086f77
Emilano <azadvararvand@gmail.com>
2022-06-28 17:18:18 +0430

    Add files via upload

---
+++ lib/mazaryn/wallet/api/cardano/address.ex
@@ -0,0 +1,8 @@
+defmodule Wallet.API.Cardano.Address do
+
+  @type address :: %{
+    id: String.t(),
+    state: String.t(),
+    derivation_path: list(String.t())
+  }
+end
---
+++ lib/mazaryn/wallet/signature.ex
@@ -0,0 +1,9 @@
+defmodule Wallet.Signature do
+
+  require Integer
+
+  def sign_transaction(_transaction, _private_key) do
+
+    :not_implemented
+  end
+end
