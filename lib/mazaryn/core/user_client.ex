8ae60f7859f1dd69dc4f0d5c3d6cb247ad6c485e
Emilano <azadvararvand@gmail.com>
2022-07-04 07:06:26 +0430

    Add files via upload

--- lib/mazaryn/core/user_client.ex
+++ lib/mazaryn/core/user_client.ex
@@ -5,8 +5,8 @@ defmodule Core.UserClient do
     :user_server.create_account(username, password, email)
   end
 
-  def login(username, password) do
-    :user_server.login(username, password)
+  def login(email, password) do
+    :user_server.login(email, password)
   end
 
   def set_user_info(username, fields, values) do
