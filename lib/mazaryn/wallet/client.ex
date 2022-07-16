16c1eae08da4b8be8b10fc3415cfe65f39aecd4e
Emilano <azadvararvand@gmail.com>
2022-07-16 13:14:30 +0430

    Add files via upload

--- lib/mazaryn/wallet/client.ex
+++ lib/mazaryn/wallet/client.ex
@@ -8,32 +8,28 @@ defmodule Wallet.Client do
     :wallet_server.create(name, password)
   end
 
-  def get_wallet(pub_key) do
-    :wallet_server.get_wallet(pub_key)
+  def get_wallet(name) do
+    :wallet_server.get_wallet(name)
   end
 
   def get_wallets() do
     :wallet_server.get_wallets()
   end
 
-  def get_address(pub_key) do
-    :wallet_server.get_address(pub_key)
+  def get_address(name) do
+    :wallet_server.get_address(name)
   end
 
-  def deposit(pub_key, amount) do
-    :wallet_server.deposit(pub_key, amount)
+  def deposit(name, amount) do
+    :wallet_server.deposit(name, amount)
   end
 
-  def withdraw(pub_key, amount) do
-    :wallet_server.withdraw(pub_key, amount)
+  def withdraw(name, amount) do
+    :wallet_server.withdraw(name, amount)
   end
 
-  def deposit(amount) do
-    :wallet_server.deposit(amount)
-  end
-
-  def get_balance(pub_key) do
-    :wallet_server.get_balance(pub_key)
+  def get_balance(name) do
+    :wallet_server.get_balance(name)
   end
 
 end
