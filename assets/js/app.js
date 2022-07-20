95ffa5a054ef1656e02243062e9af19ac255f78a
pau <jethro.riosa@gmail.com>
2022-07-20 23:33:45 +0800

    remove non existing module

--- assets/js/app.js
+++ assets/js/app.js
@@ -24,7 +24,6 @@ import "phoenix_html";
 import { Socket } from "phoenix";
 import { LiveSocket } from "phoenix_live_view";
 import topbar from "../vendor/topbar";
-import { Select } from "./select";
 
 let Hooks = {};
 
