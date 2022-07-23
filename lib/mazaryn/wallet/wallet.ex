fffcccbbdcef45b1716c855224c15bc52b4afba4
Emilano <azadvararvand@gmail.com>
2022-07-23 09:37:49 +0430

    Add files via upload

--- lib/mazaryn/wallet/wallet.ex
+++ lib/mazaryn/wallet/wallet.ex
@@ -1,43 +1,15 @@
 defmodule Mazaryn.Wallet do
-  use Ecto.Schema
 
-  import Ecto.Changeset
-
-  # defstruct [
-  #  name: nil,
-  #  password: nil,
-  #  address: nil,
-  #  balance: nil,
-  #  pub_key: nil,
-  #  priv_key: nil,
-  #  tokens: nil
-  # ]
-
-  # def new(name, password) do
-  #  %__MODULE__{
-  #  name: name,
-  #  password: password
-  #  }
-  # end
-
-  schema "wallets" do
-    field(:name, :string)
-    field(:username, :string)
-    field(:password)
-    has_many(:transactions, Mazaryn.Wallet.Transaction)
-    belongs_to(:user, Account.User)
-
-    # timestamps()
-  end
-
-  @required_attrs [
+  @doc false
+  defstruct [
     :name,
     :password
   ]
 
-  def changeset(wallet, params \\ %{}) do
-    wallet
-    |> cast(params, @required_attrs)
-    |> validate_required(@required_attrs)
+  def new(name, password) do
+    %__MODULE__{
+      name: name,
+      password: password
+    }
   end
 end
