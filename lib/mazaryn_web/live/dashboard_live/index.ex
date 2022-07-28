a63d47b4c35391e74466eb9b744670f4f3728bef
Emilano <azadvararvand@gmail.com>
2022-07-28 16:09:25 +0430

    Add files via upload

--- lib/mazaryn_web/live/dashboard_live/index.ex
+++ lib/mazaryn_web/live/dashboard_live/index.ex
@@ -1,3 +1,16 @@
 defmodule DashboardLive.Index do
   use MazarynWeb, :live_view
+
+  import MazarynWeb.Live.Helper
+  import Logger
+
+  alias Account.Users
+
+  # case reload dashboard page
+  @impl true
+  def mount(_params, %{"user_id" => user_id} = _session, socket) do
+    Logger.info(user_id: user_id)
+    {:ok, do_mount(user_id, socket)}
+  end
+
 end
