24e47d430d82f1e96b592da28ac7ed62ac494836
Emilano <azadvararvand@gmail.com>
2022-07-28 18:14:43 +0430

    Add files via upload

--- lib/mazaryn_web/live/dashboard_live/index.ex
+++ lib/mazaryn_web/live/dashboard_live/index.ex
@@ -5,6 +5,9 @@ defmodule DashboardLive.Index do
   import Logger
 
   alias Account.Users
+  alias Wallet.Client
+  alias Dashboard.Wallet
+  alias Mazaryn.Wallet
 
   # case reload dashboard page
   @impl true
@@ -12,5 +15,15 @@ defmodule DashboardLive.Index do
     Logger.info(user_id: user_id)
     {:ok, do_mount(user_id, socket)}
   end
+
+  defp do_mount(user_id, socket) do
+    wallet_changeset = Wallet.changeset(%Wallet{})
+
+    socket
+    |> assign(wallet_changeset: wallet_changeset)
+    |> assign(posts: get_wallet())
+  end
+
+  def get_wallet, do: Wallet.Client.get_wallets()
 
 end
