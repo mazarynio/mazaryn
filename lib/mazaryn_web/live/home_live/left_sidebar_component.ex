91f20f7953d478d79ae110b25ebe4a2cd1ce2933
Emilano <azadvararvand@gmail.com>
2022-07-29 18:00:34 +0430

    Add files via upload

--- lib/mazaryn_web/live/home_live/left_sidebar_component.ex
+++ lib/mazaryn_web/live/home_live/left_sidebar_component.ex
@@ -48,7 +48,6 @@ defmodule MazarynWeb.HomeLive.LeftSidebarComponent do
                 </li>
                 <li class="flex align-center items-center group">
                     <%= live_patch to: Routes.live_path(@socket, MazarynWeb.DashboardLive.Index), replace: false, class: "flex items-center text-1 px-2 py-4 text-gray-500 font-semibold group-hover:text-blue-500" do %>
-                        <i><%= Heroicons.icon("dashboard", class: "h-5 w-5 mr-3") %></i>
                         Dashboard
                     <% end %>
                 </li>
