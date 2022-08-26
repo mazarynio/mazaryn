0b0fb2a4ead633784d31974216b03704a48356eb
Arvand Azadvar <azadvararvand@gmail.com>
2022-08-26 16:04:43 +0430

    Add files via upload

--- src/otpcode/src/wallet/aeternity/ae_wallet_server.erl
+++ src/otpcode/src/wallet/aeternity/ae_wallet_server.erl
@@ -1,5 +1,5 @@
 -module(ae_wallet_server).
--export([start_link/0, create/1]).
+-export([start_link/0, create/3]).
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2,
   code_change/3]).
 
@@ -10,17 +10,17 @@
 -record(state, {}).
 
 start_link() ->
-  ?LOG_NOTICE("Wallet server has been started - ~p", [self()]),
+  ?LOG_NOTICE("Aeternity Wallet server has been started - ~p", [self()]),
   gen_server:start_link({global, ?MODULE}, ?MODULE, [], []).
 
-create(Size) ->
-  gen_server:call({global, ?MODULE}, {create, Size}).
+create(Name, Password, Size) ->
+  gen_server:call({global, ?MODULE}, {create, Name, Password, Size}).
 
 init([]) ->
   {ok, []}.
 
-handle_call({create, Size}, _From, State) ->
-  Address = ae_walletdb:insert(Size),
+handle_call({create, Name, Password, Size}, _From, State) ->
+  Address = ae_walletdb:insert(Name, Password, Size),
   {reply, Address, State};
 
 handle_call(_Request, _From, State) ->
