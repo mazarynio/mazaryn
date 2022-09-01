3c8096ab330ae026d3e41c34b88e85e152a3c2d7
Oliver Favalli <oliver.hfavalli@gmail.com>
2022-09-01 20:48:27 -0300

    feat: horizontal and in-article ads added for all blog posts

--- lib/mazaryn/account/user_notifier.ex
+++ lib/mazaryn/account/user_notifier.ex
@@ -1,50 +1,50 @@
-defmodule Account.UserNotifier do
-  use Phoenix.Swoosh, view: MazarynWeb.UserNotifierView
+# defmodule Account.UserNotifier do
+#   use Phoenix.Swoosh, view: MazarynWeb.UserNotifierView
 
-  def welcome(user) do
-    new()
-    |> from("no-reply@mazaryn.io")
-    |> to(user.email)
-    |> subject("mail verification")
-    |> render_body("welcome.html", %{name: name})
-  end
+#   def welcome(user) do
+#     new()
+#     |> from("no-reply@mazaryn.io")
+#     |> to(user.email)
+#     |> subject("mail verification")
+#     |> render_body("welcome.html", %{name: name})
+#   end
 
-  defp deliver(to, body) do
-    require Logger
-    Logger.debug(body)
-    {:ok, %{to: to, body: body}}
-  end
+#   defp deliver(to, body) do
+#     require Logger
+#     Logger.debug(body)
+#     {:ok, %{to: to, body: body}}
+#   end
 
-  def deliver_confirmation_instructions(user, url) do
-    deliver(user.email, """
-    ==============================
-    Hi #{user.email},
-    You can confirm your account by visiting the URL below:
-    #{url}
-    If you didn't create an account with us, please ignore this.
-    ==============================
-    """)
-  end
+#   def deliver_confirmation_instructions(user, url) do
+#     deliver(user.email, """
+#     ==============================
+#     Hi #{user.email},
+#     You can confirm your account by visiting the URL below:
+#     #{url}
+#     If you didn't create an account with us, please ignore this.
+#     ==============================
+#     """)
+#   end
 
-  def deliver_reset_password_instructions(user, url) do
-    deliver(user.email, """
-    ==============================
-    Hi #{user.email},
-    You can reset your password by visiting the URL below:
-    #{url}
-    If you didn't request this change, please ignore this.
-    ==============================
-    """)
-  end
+#   def deliver_reset_password_instructions(user, url) do
+#     deliver(user.email, """
+#     ==============================
+#     Hi #{user.email},
+#     You can reset your password by visiting the URL below:
+#     #{url}
+#     If you didn't request this change, please ignore this.
+#     ==============================
+#     """)
+#   end
 
-  def deliver_update_email_instructions(user, url) do
-    deliver(user.email, """
-    ==============================
-    Hi #{user.email},
-    You can change your email by visiting the URL below:
-    #{url}
-    If you didn't request this change, please ignore this.
-    ==============================
-    """)
-  end
-end
+#   def deliver_update_email_instructions(user, url) do
+#     deliver(user.email, """
+#     ==============================
+#     Hi #{user.email},
+#     You can change your email by visiting the URL below:
+#     #{url}
+#     If you didn't request this change, please ignore this.
+#     ==============================
+#     """)
+#   end
+# end
