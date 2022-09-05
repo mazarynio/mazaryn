8f89bb49a02c9d79181fe56e4f8a799b52b8c33c
Arvand Azadvar <azadvararvand@gmail.com>
2022-09-05 16:45:51 +0430

    Add files via upload

---
+++ lib/mazaryn/rustcode/rustcode.ex
@@ -0,0 +1,11 @@
+defmodule Elixir.Rustcode do
+  use Rustler,
+    otp_app: :mazaryn,
+    crate: :rustcode
+
+    def add(_a, _b), do: nif()
+
+    defp nif() do
+      :erlang.nif_error(:nif_not_loaded)
+    end
+end
