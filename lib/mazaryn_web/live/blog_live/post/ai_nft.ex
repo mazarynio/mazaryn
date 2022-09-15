eacdbcab70eb6cf4d58a5a96babd4005855c6622
Arvand Azadvar <azadvararvand@gmail.com>
2022-09-15 16:22:32 +0430

    Add files via upload

---
+++ lib/mazaryn_web/live/blog_live/post/ai_nft.ex
@@ -0,0 +1,14 @@
+defmodule MazarynWeb.BlogLive.Post.AINFT do
+  use MazarynWeb, :live_view
+
+  alias Phoenix.LiveView.JS
+
+  def mount(_params, _session, socket) do
+    {:ok, socket}
+  end
+
+  def toggle_menu(js \\ %JS{}) do
+    js
+    |> JS.toggle(to: ".menu-content", in: "fade-overflow")
+  end
+end
