ea99fbca2259996fca57477525981cbe60ec8d74
pau <jethro.riosa@gmail.com>
2022-09-19 06:45:08 +0800

    downgrade to elixir 1.13

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Mazaryn.MixProject do
     [
       app: :mazaryn,
       version: "0.1.0",
-      elixir: "~> 1.14",
+      elixir: "~> 1.13",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
