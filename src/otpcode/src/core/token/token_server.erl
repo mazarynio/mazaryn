a622beea6afd3c3aeb4514daf70ae4be5b06fceb
Arvand Azadvar <azadvararvand@gmail.com>
2022-10-24 19:08:51 +0330

    Add files via upload

---
+++ src/otpcode/src/core/token/token_server.erl
@@ -0,0 +1,42 @@
+-module(token_server).
+
+-include_lib("kernel/include/logger.hrl").
+
+-export([start_link/0, validate]).
+
+%% gen_server callbacks
+-export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2,
+  code_change/3]).
+
+-behaviour(gen_server).
+-define(SERVER, ?MODULE).
+-record(state, {}).
+
+start_link() ->
+    gen_server:start_link({global, ?SERVER}, ?MODULE, [], []). 
+
+validate(TokenID) ->
+    gen_server:call({global, ?MODULE}, {validate, TokenID}).
+
+init([]) ->
+    ?LOG_NOTICE("Token server has been started - ~p", [self()]),
+    {ok, #state{}}.
+
+handle_call({validate, TokeID}, _From, State) ->
+    Token = userdb:get_token_by_id(TokenID),
+    {reply, Token, State};
+
+handle_call(_Request, _From, State) ->
+    {noreply, State}.
+
+handle_cast(_Request, State) ->
+    {noreply, State}.
+
+handle_info(_Info, State) ->
+    {noreply, State}.
+
+terminate(_Reason, _State) ->
+    ok.
+
+code_change(_OldVsn, State, _Extra) ->
+    {ok, State}.
