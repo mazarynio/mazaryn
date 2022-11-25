299eec58a112c10359d4aa3ba5d0483d9fc90944
Arvand Azadvar <arvandazadvar@gmail.com>
2022-11-25 22:35:45 +0330

    Create hedera_walletdb.erl

---
+++ src/otpcode/src/wallet/hedera/hedera_walletdb.erl
@@ -0,0 +1,16 @@
+-module(hedera_walletdb).
+-export([insert/1]).
+-record(hed_wallet, {id, password, date_created}).
+
+insert(Password) ->
+  Fun = fun() ->
+    Now = calendar:universal_time(),
+    Id = id_gen:generate(),
+    Hed_wallet = #hed_wallet{id = Id,
+                        password = erlpass:hash(Password),
+                        date_created = Now},
+    mnesia:write(Hed_wallet),
+    Id
+  end,
+  {atomic, Res} = mnesia:transaction(Fun),
+  Res.
