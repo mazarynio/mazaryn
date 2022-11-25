1a44760ef9fc2094b32db3311dac91588fcecb67
Arvand Azadvar <arvandazadvar@gmail.com>
2022-11-25 23:00:08 +0330

    Create hedera_wallet_server.erl

---
+++ src/otpcode/src/wallet/hedera/hedera_wallet_server.erl
@@ -0,0 +1,42 @@
+-module(hedera_wallet_server).
+
+-include_lib("kernel/include/logger.hrl").
+
+-export([start_link/0, create_account/1]).
+
+%% gen_server callbacks
+-export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2,
+  code_change/3]).
+
+-behaviour(gen_server).
+-define(SERVER, ?MODULE).
+-record(state, {}).
+
+start_link() ->
+    gen_server:start_link({global, ?SERVER}, ?MODULE, [], []).
+
+create_account(Password) ->
+    gen_server:call({global, ?MODULE}, {create_account, Password}).
+
+init([]) ->
+    ?LOG_NOTICE("User server has been started - ~p", [self()]),
+    {ok, #state{}}.
+
+handle_call({create_account, Password}, _From, State) ->
+    Id = hedera_walletdb:insert(Password),
+    {reply, Id, State};
+
+handle_call(_Request, _From, State) ->
+    {noreply, State}.
+
+handle_cast(_Request, State) ->
+    {noreply, State}.
+
+handle_info(_Info, State) ->
+    {noreply, State}.
+
+terminate(_Reason, _State) ->
+    ok.
+
+code_change(_OldVsn, State, _Extra) ->
+    {ok, State}.
