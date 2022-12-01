131a7f0e9a5a98deba602df6dc16ad785c43dd65
pau <jethro.riosa@gmail.com>
2022-12-01 23:32:59 +0800

    added check origin

--- config/runtime.exs
+++ config/runtime.exs
@@ -61,7 +61,8 @@ if config_env() == :prod do
       ip: {0, 0, 0, 0, 0, 0, 0, 0},
       port: port
     ],
-    secret_key_base: secret_key_base
+    secret_key_base: secret_key_base,
+    check_origin: ["https://mazaryn.io", "https://mazaryn.fly.dev"]
 
   config :mazaryn, :media,
     uploads_dir: "/app/bin/uploads",
