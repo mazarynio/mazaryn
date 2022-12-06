c660b263dd8feea790d3b6e430942a9a199c3750
Arvand Azadvar <azadvararvand@gmail.com>
2022-12-06 17:42:36 +0330

    Add files via upload

--- lib/mazaryn/wallet/hedera/hed_wallet_client.ex
+++ lib/mazaryn/wallet/hedera/hed_wallet_client.ex
@@ -8,6 +8,10 @@ defmodule Wallet.Hedera.HedWalletClient do
     :hedera_wallet_server.create_account(password)
   end
 
+  def get_wallet_by_id(id) do
+    :hedera_wallet_server.get_wallet_by_id(id)
+  end
+
   def get_wallets() do
     :hedera_wallet_server.get_wallets()
   end
