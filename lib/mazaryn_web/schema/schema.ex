6ed5837690061048feff0f7397a1a144b42e4f94
Arvand Azadvar <azadvararvand@gmail.com>
2022-12-06 17:08:32 +0330

    Add files via upload

--- lib/mazaryn_web/schema/schema.ex
+++ lib/mazaryn_web/schema/schema.ex
@@ -4,6 +4,10 @@ defmodule MazarynWeb.Schema do
   import_types(MazarynWeb.Schema.Post)
   import_types(MazarynWeb.Schema.HederaWallet)
 
+  alias Resolvers.PostResolver
+  alias Resolvers.UserResolver
+  alias Resolvers.WalletResolvers
+
   query do
     @desc "Get all users"
     field :users, list_of(:user) do
