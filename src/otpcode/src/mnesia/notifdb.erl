745ad6e6246f1b33a2d5bef3454066d0e9056b03
Arvand Azadvar <arvandazadvar@proton.me>
2023-01-03 16:23:48 +0330

    Add files via upload

--- src/otpcode/src/mnesia/notifdb.erl
+++ src/otpcode/src/mnesia/notifdb.erl
@@ -1,38 +1,20 @@
 -module(notifdb).
--export([insert/1, insert/3, get_notif_by_id/1]).
+-export([insert/2]).
 
 -include("../records.hrl").
 -include_lib("stdlib/include/qlc.hrl").
 
-insert(Message) ->
-    Id = nanoid:gen(),
-    Notification = #notification{id = Id,
-                                 message = Message},
+insert(To, Message) ->
     F = fun() ->
-        mnesia:write(Notification)
-    end,
-    mnesia:transaction(F).
-
-insert(From, To, Message) ->
-    Fun = fun() ->
         Id = nanoid:gen(),
-        Notification = #notification{id = Id,
-                                     from = From,
-                                     to = To,
-                                     message = Message},
-        mnesia:write(Notification),
-        Id
+        mnesia:write(#notification{id = Id,
+                                   message = Message,
+                                   to = To,
+                                   date_created = calendar:universal_time() }),
+          [User] = mnesia:index_read(user, To, username),
+          Notifications = User#user.notification,
+          mnesia:write(User#user{notification = [Id | Notifications]}),
+          Id
     end,
-    {atomic, Res} = mnesia:transaction(Fun),
+    {atomic, Res} = mnesia:transaction(F),
     Res.
-
-get_notif_by_id(Id) ->
-    Res = mnesia:transaction(
-            fun() ->
-                mnesia:match_object(#notification{id = Id, _= '_'})
-            end),
-    case Res of
-      {atomic, []} -> notif_not_exist;
-      {atomic, [Notification]} -> Notification;
-      _ -> error
-    end.
