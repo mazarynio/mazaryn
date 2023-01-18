66017c6e9f366a93c773e89e34e3e9129423ddc0
pau <jethro.riosa@gmail.com>
2023-01-18 23:13:27 +0800

    remove unnecessary images

--- priv/static/images/index-21f4029c0d169ade0569098a95e3916e.js
+++
@@ -1,9 +0,0 @@
-export { default as logo } from "./logo.png";
-export { default as profileImage } from "./profileImage.png";
-export { default as mazaryn } from "./mazaryn.png";
-export { default as dummyPost } from "./dummypost.png";
-export { default as like } from "./like.svg";
-export { default as dislike } from "./dislike.svg";
-export { default as comment } from "./comment.svg";
-export { default as share } from "./share.svg";
-export { default as dollar } from "./dollar.svg";
