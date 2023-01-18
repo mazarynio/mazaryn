d254337e0ae37263c40b771309052637562dcc8c
Arvand Azadvar <arvandazadvar@proton.me>
2023-01-18 10:12:48 +0330

    Add files via upload

--- src/otpcode/src/mnesia/notifdb.erl
+++ src/otpcode/src/mnesia/notifdb.erl
@@ -1,10 +1,10 @@
 -module(notifdb).
--export([insert/2]). 
+-export([insert/2, get_single_notif/1, get_all_notifs/1]). 
 
 -include("../records.hrl").
 -include_lib("stdlib/include/qlc.hrl").
 
-insert(UserID, Message) ->
+insert(UserID, Message) -> 
     Fun = fun() ->
             Id = nanoid:gen(),
             mnesia:write(#notif{id = Id,
@@ -18,4 +18,27 @@ insert(UserID, Message) ->
           end,
     {atomic, Res} = mnesia:transaction(Fun),
     Res.
+
+get_single_notif(NotifID) ->
+    Fun = fun() ->
+            [Notif] = mnesia:read({notif, NotifID}),
+            Notif
+          end,
+    {atomic, Res} = mnesia:transaction(Fun),
+    Res.
+
+get_all_notifs(UserID) ->
+  Fun = fun() ->
+          mnesia:match_object(#notif{user_id = UserID,
+                                              _ = '_'}),
+          [User] = mnesia:read({user, UserID}),
+          lists:foldl(fun(ID, Acc) ->
+                        [Notif] = mnesia:read({notif, ID}),
+                        [Notif|Acc]
+                      end,
+                      [], User#user.notif)
+
+        end,
+  {atomic, Res} = mnesia:transaction(Fun),
+  Res.
     
