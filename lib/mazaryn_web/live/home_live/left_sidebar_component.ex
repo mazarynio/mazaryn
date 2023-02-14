f116dc75fc2f946b0e2d976f0abbcfb56f3c05cd
pau-riosa <jethro.riosa@gmail.com>
2023-02-14 09:45:04 +0800

    fix left side-bar component [deploy fly]

--- lib/mazaryn_web/live/home_live/left_sidebar_component.ex
+++ lib/mazaryn_web/live/home_live/left_sidebar_component.ex
@@ -19,7 +19,7 @@ defmodule MazarynWeb.HomeLive.LeftSidebarComponent do
                             <% end %>
                         </li>
                         <li class="flex align-center items-center mx-2 mb-7">
-                            <a href="index.html" class="group flex items-start text-base text-gray-500 font-semibold hover:text-blue-500 ">
+                            <a href="/" class="group flex items-start text-base text-gray-500 font-semibold hover:text-blue-500 ">
                                 <i>
                                     <svg class="h-5 w-5 mr-3.5 group-hover:fill-[#4385F5]" width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
                                         <g clip-path="url(#clip0_390_1350)">
