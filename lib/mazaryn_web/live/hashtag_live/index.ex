c35fa4b4b02016c4f532e40ac439191eaf4320d3
TraceyOnim <traceyonim22@gmail.com>
2023-07-12 15:43:31 +0300

    styling the hashtag page

--- lib/mazaryn_web/live/hashtag_live/index.ex
+++ lib/mazaryn_web/live/hashtag_live/index.ex
@@ -6,7 +6,7 @@ defmodule MazarynWeb.HashtagLive.Index do
 
   def mount(_params, %{"session_uuid" => session_uuid} = _session, socket) do
     {:ok, user} = Users.get_by_session_uuid(session_uuid)
-    {:ok, assign(socket, current_user: user)}
+    {:ok, assign(socket, current_user: user, search: "")}
   end
 
   def handle_params(%{"hashtag_name" => hashtag}, _uri, socket) do
@@ -16,5 +16,11 @@ defmodule MazarynWeb.HashtagLive.Index do
 
   def handle_params(_params, _uri, socket) do
     {:noreply, socket}
+  end
+
+  def handle_event("do_search", %{"search" => search}, socket) do
+    socket = assign(socket, search: search)
+
+    {:noreply, socket |> push_redirect(to: Routes.live_path(socket, MazarynWeb.SearchLive.Index))}
   end
 end
