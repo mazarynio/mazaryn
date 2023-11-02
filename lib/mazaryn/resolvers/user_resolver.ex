8254d3fa9fb627d665a7874b6b1d6bd6546e6d02
cheropChirchir7963 <cheropmarlene@gmail.com>
2023-11-02 12:18:32 +0300

    fix graphql users query

--- lib/mazaryn/resolvers/user_resolver.ex
+++ lib/mazaryn/resolvers/user_resolver.ex
@@ -2,7 +2,7 @@ defmodule Resolvers.UserResolver do
   alias Core.UserClient, as: UserClient
 
   def all(_args, _info) do
-    {:ok, UserClient.get_all()}
+    {:ok, UserClient.get_all_users_info_by_ids()}
   end
 
   def create_user(username, password, email) do
