dd5ad332ff15c0ee7abef1d5f2e48afa738b0119
amos-kibet <kibetamos97@gmail.com>
2024-01-03 15:32:27 +0300

    Avoid confusion with default postgres database

--- config/dev.exs
+++ config/dev.exs
@@ -4,7 +4,7 @@ config :mazaryn, Mazaryn.Repo,
   username: "postgres",
   password: "postgres",
   hostname: "localhost",
-  database: "postgres",
+  database: "mazaryn_dev",
   stacktrace: true,
   show_sensitive_data_on_connection_error: true,
   pool_size: 10
