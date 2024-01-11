48f7658c4427ebdecc356d21d73581ad077bb990
emilano <arvandazadvar@proton.me>
2024-01-11 10:50:24 +0330

    db

--- config/config.exs
+++ config/config.exs
@@ -70,4 +70,4 @@ config :phoenix, :json_library, Jason
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{config_env()}.exs"
-config :mnesia, dir: ~c"Mnesia/"
+config :mnesia, dir: 'Mnesia/'
