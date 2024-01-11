cc036bb43ecb9756737a13c9a365678a7fc6f651
emilano <arvandazadvar@proton.me>
2024-01-11 10:27:33 +0330

    fixxx

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Mazaryn.MixProject do
     [
       app: :mazaryn,
       version: "0.1.0",
-      elixir: "~> 1.15.5",
+      elixir: "~> 1.16.0",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
