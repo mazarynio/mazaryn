681c617fa69c424a3836b8f6f8dd63c2983c444d
emilano <emilano@localhost.localdomain>
2024-04-21 02:20:23 +0330

    level management

---
+++ src/otpcode/src/core/user/user_level_db.erl
@@ -0,0 +1,33 @@
+-module(user_level_db).
+-author("Zaryn Technologies").
+-include("../../records.hrl").
+-export([remove_level_one_users/0, downgrade_level_two_users/0]).
+
+remove_level_one_users() ->
+    Fun = fun() ->
+        CurrentTime = erlang:system_time(seconds),
+        SixMonthsAgo = CurrentTime - (6 * 30 * 24 * 60 * 60),
+        MatchSpec = [{#user{level = 1, date_created = '$1', _ = '_'},
+                      [{'=<', '$1', SixMonthsAgo}],
+                      ['$_']}],
+        Users = mnesia:select(user, MatchSpec),
+        lists:foreach(fun(User) -> mnesia:delete_object(User) end, Users)
+    end,
+    {atomic, _} = mnesia:transaction(Fun),
+    ok.
+
+downgrade_level_two_users() ->
+    Fun = fun() ->
+        CurrentTime = erlang:system_time(seconds),
+        SixMonthsAgo = CurrentTime - (6 * 30 * 24 * 60 * 60),
+        MatchSpec = [{#user{level = 2, date_updated = '$1', _ = '_'},
+                      [{'=<', '$1', SixMonthsAgo}],
+                      ['$_']}],
+        Users = mnesia:select(user, MatchSpec),
+        lists:foreach(fun(User) ->
+                          UpdatedUser = User#user{level = 1},
+                          mnesia:write(UpdatedUser)
+                      end, Users)
+    end,
+    {atomic, _} = mnesia:transaction(Fun),
+    ok.
---
+++ src/otpcode/src/core/user/user_level_server.erl
@@ -0,0 +1,48 @@
+-module(user_level_server).
+-author("Zaryn Technologies").
+-export([start_link/0, remove_level_one_users/0, downgrade_level_two_users/0]).
+
+%% gen_server callbacks
+-export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2,
+  code_change/3]).
+
+-behaviour(gen_server).
+-define(SERVER, ?MODULE).
+-record(state, {}).
+-include_lib("kernel/include/logger.hrl").
+
+start_link() ->
+    gen_server:start_link({global, ?SERVER}, ?MODULE, [], []).
+
+remove_level_one_users() ->
+    gen_server:call({global, ?MODULE}, {remove_level_one_users}).
+
+downgrade_level_two_users() ->
+    gen_server:call({global, ?MODULE}, {downgrade_level_two_users}).
+
+init([]) ->
+    ?LOG_NOTICE("User auto level management has been started - ~p", [self()]),
+    {ok, #state{}}.
+
+handle_call({remove_level_one_users}, _From, State) ->
+    Res = user_level_db:remove_level_one_users(),
+    {reply, Res, State};
+
+handle_call({downgrade_level_two_users}, _From, State) ->
+    Res = user_level_db:downgrade_level_two_users(),
+    {reply, Res, State};
+
+handle_call(_Request, _From, State) ->
+    {noreply, State}.
+
+handle_cast(_Request, State) ->
+    {noreply, State}.
+
+handle_info(_Info, State) ->
+    {noreply, State}.
+
+terminate(_Reason, _State) ->
+    ok.
+
+code_change(_OldVsn, State, _Extra) ->
+    {ok, State}.
