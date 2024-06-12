e316cc5f0734e53e81210c48c791bd5bdacf43e0
Fredrick Oluoch <fredrick@tectonagroup.com>
2024-06-12 03:21:14 +0300

    Table with only email and username

--- lib/mazaryn_web/live/user_live/manage.ex
+++ lib/mazaryn_web/live/user_live/manage.ex
@@ -5,20 +5,36 @@ defmodule MazarynWeb.UserLive.Manage do
 
   alias Account.User
   alias Account.Users
+  alias ManageUser
 
   def mount(_params, %{"session_uuid" => session_uuid} = _session, socket) do
     {:ok, current_user} = Users.get_by_session_uuid(session_uuid)
 
+    # users = Users.get_all_users() |> IO.inspect(label: "mbogi")
+    users_info_list = ManageUser.get_users_info();
+    # users_info_list |> IO.inspect(label: "mbogi")
+
+    users = fetch_data(users_info_list)
+    users |> IO.inspect(label: "genje mbogi")
     socket =
       socket
       |> assign(title: "Manage Users")
       |> assign(session_uuid: session_uuid)
       |> assign(current_user: current_user)
       |> assign(search: nil)
-
-
+      |> assign(users: users)
 
     {:ok, assign(socket, %{})}
+  end
+
+  def fetch_data(data) do
+    data |> IO.inspect(label: "kekekeke")
+
+    Enum.map(data, fn
+      {:user, _,username,password_hash,email,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_} -> %{email: email, username: username, password_hash: password_hash}
+      # {:user,
+      #  _, _, _, email, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _} -> email
+    end)
   end
 
 end
