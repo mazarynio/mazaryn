091a781005e2a05fd12321a8906fcec0488de5ad
Fredrick Oluoch <fredrick@tectonagroup.com>
2024-06-12 04:53:56 +0300

    Code cleanup

--- lib/mazaryn_web/live/user_live/manage.ex
+++ lib/mazaryn_web/live/user_live/manage.ex
@@ -10,31 +10,66 @@ defmodule MazarynWeb.UserLive.Manage do
   def mount(_params, %{"session_uuid" => session_uuid} = _session, socket) do
     {:ok, current_user} = Users.get_by_session_uuid(session_uuid)
 
-    # users = Users.get_all_users() |> IO.inspect(label: "mbogi")
-    users_info_list = ManageUser.get_users_info();
-    # users_info_list |> IO.inspect(label: "mbogi")
+    users_info_list = ManageUser.get_users_info()
 
     users = fetch_data(users_info_list)
-    users |> IO.inspect(label: "genje mbogi")
+
     socket =
       socket
       |> assign(title: "Manage Users")
       |> assign(session_uuid: session_uuid)
       |> assign(current_user: current_user)
       |> assign(search: nil)
-      |> assign(users: users)
+      |> assign(users: users |> sort_by_date())
 
     {:ok, assign(socket, %{})}
   end
 
-  def fetch_data(data) do
-    data |> IO.inspect(label: "kekekeke")
-
-    Enum.map(data, fn
-      {:user, _,username,password_hash,email,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_} -> %{email: email, username: username, password_hash: password_hash}
-      # {:user,
-      #  _, _, _, email, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _} -> email
+  def sort_by_date(list) when is_list(list) do
+    Enum.sort(list, fn %{last_activity: date1}, %{last_activity: date2} ->
+      NaiveDateTime.from_iso8601!(date2) >= NaiveDateTime.from_iso8601!(date1)
     end)
   end
 
+  def fetch_data(data) do
+    Enum.map(data, fn
+      {:user, id, username, password_hash, email, address, knode, media, post, blog_post, notif,
+       following, follower, blocked, saved_posts, other_info, private, date_created, date_updated,
+       avatar_url, banner_url, token_id, chat, verified, report, level, last_activity, suspend,
+       data} ->
+        naive_datetime = NaiveDateTime.from_erl!(last_activity)
+        human_readable_time = NaiveDateTime.to_string(naive_datetime)
+
+        %{
+          id: id,
+          username: username,
+          password: password_hash,
+          email: email,
+          address: address,
+          knode: knode,
+          media: media,
+          post: post,
+          blog_post: blog_post,
+          notif: notif,
+          following: following,
+          follower: follower,
+          blocked: blocked,
+          saved_posts: saved_posts,
+          other_info: other_info,
+          private: private,
+          date_created: date_created,
+          date_updated: date_updated,
+          avatar_url: avatar_url,
+          banner_url: banner_url,
+          token_id: token_id,
+          chat: chat,
+          verified: verified,
+          report: report,
+          level: level,
+          last_activity: human_readable_time,
+          suspend: suspend,
+          data: data
+        }
+    end)
+  end
 end
