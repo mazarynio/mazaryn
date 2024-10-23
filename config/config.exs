981d5037bf1933b72986638e6257339bf945ef61
emilano <arvandazadvar@proton.me>
2024-10-23 20:57:43 +0330

    add more lng

--- config/config.exs
+++ config/config.exs
@@ -75,7 +75,7 @@ config :mnesia, dir: ~c"Mnesia/"
 
 config :mazaryn, Mazaryn.Gettext,
   default_locale: "en",
-  locales: ["en", "fa", "ru"]
+  locales: ["en", "fa", "ru", "zh", "az"]
 
 config :mazaryn, :media,
   uploads_dir: Path.join([File.cwd!(), "priv", "static", "uploads"])
