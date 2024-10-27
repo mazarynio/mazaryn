5a8109c6ebbe70c2046d5a08bc0ba08dd2802aa0
emilano <arvandazadvar@proton.me>
2024-10-27 19:59:34 +0330

    more locale

--- config/config.exs
+++ config/config.exs
@@ -75,7 +75,7 @@ config :mnesia, dir: ~c"Mnesia/"
 
 config :mazaryn, Mazaryn.Gettext,
   default_locale: "en",
-  locales: ["en", "fa", "ru", "zh", "az", "ar", "es", "vi", "hi"]
+  locales: ["en", "fa", "ru", "zh", "az", "ar", "es", "vi", "hi", "de", "fr", "it", "pt"]
 
 config :mazaryn, :media,
   uploads_dir: Path.join([File.cwd!(), "priv", "static", "uploads"])
