b7ea725ba907b3d428d6cbed6143312fb21f98b9
OkallGordon <gordonochieng5454@gmail.com>
2025-04-19 18:30:13 +0300

    Improve notification handling, auto-hide on /notifications page and click

--- lib/mazaryn_web/live/home_live/notification.ex
+++ lib/mazaryn_web/live/home_live/notification.ex
@@ -2,23 +2,23 @@ defmodule MazarynWeb.HomeLive.Notification do
   use MazarynWeb, :live_view
   alias Account.Users
 
-  # case reload home page
   @impl true
   def mount(_params, %{"user_id" => user_email} = _session, socket) do
-    Process.send_after(self(), :time_diff, 1000)
-    {:ok, user} = Users.one_by_email(user_email)
+  Process.send_after(self(), :time_diff, 1000)
+  {:ok, user} = Users.one_by_email(user_email)
 
-    {:ok,
-     socket
-     |> assign(target_user: user)
-     |> assign(search: "")
-     |> assign(notifs: get_all_user_notifs(user))}
+  {:ok,
+   socket
+   |> assign(target_user: user)
+   |> assign(search: "")
+   |> assign(notifs: get_all_user_notifs(user))
+   |> assign(notification_visible: true)
+   |> assign(hide_notification_icon: true)}
   end
 
   @impl true
   def handle_params(_params, url, socket) do
     socket = assign(socket, current_path: URI.parse(url).path)
-
     IO.inspect("this is this is workin")
     MazarynWeb.HomeLive.NavComponent.handle_path(socket)
   end
@@ -28,12 +28,14 @@ defmodule MazarynWeb.HomeLive.Notification do
     ~H"""
     <!-- navigation -->
     <.live_component
-      module={MazarynWeb.HomeLive.NavComponent}
-      id="navigation"
-      user={@target_user}
-      search={@search}
-      locale={@locale}
-    />
+     module={MazarynWeb.HomeLive.NavComponent}
+     id="navigation"
+     user={@target_user}
+     search={@search}
+     locale={@locale}
+     current_path={@current_path}
+    /> 
+
     <!-- Three columns -->
     <div class="bg-[#FAFAFA]">
       <div class="flex flex-wrap w-[90%] max-w-[1440px] mx-auto">
@@ -45,38 +47,42 @@ defmodule MazarynWeb.HomeLive.Notification do
             locale={@locale}
           />
         </div>
-
         <div class="w-full lg:w-[54%] py-6 pl-11 pr-8">
           <div class="flex flex-wrap justify-center align-center mb-6">
-            <div class="w-full bg-white white:bg-gray-800 custom-box-shadow pr-[1.35rem] pl-[1.6rem] pb-2 pt-5 mt-8 rounded-[20px]">
-              <%= for {user, message, time_passed, _time_stamp} <- @notifs do %>
-                <div class="flex justify-between align-center items-center mb-5">
-                  <div class="flex justify-center items-center">
-                    <img
-                      class="h-11 w-11 rounded-full"
-                      src={user.avatar_url || "/images/default-user.svg"}
-                    />
-                    <div class="ml-3.5 text-sm leading-tight mt-5">
-                      <span class="block text-[#60616D] text-sm">
-                        <a
-                          class="text-blue-500"
-                          href={
-                            Routes.live_path(
-                              @socket,
-                              MazarynWeb.UserLive.Profile,
-                              user.username,
-                              @locale
-                            )
-                          }
-                        >
-                          <%= user.username %>
-                        </a>
-                      </span>
-                      <span class="block text-[#60616D] text-sm"><%= message %></span>
-                      <span class="block text-[#60616D] text-sm"><%= time_passed %></span>
+            <div class="w-full bg-white custom-box-shadow pr-[1.35rem] pl-[1.6rem] pb-2 pt-5 mt-8 rounded-[20px]">
+              <!-- 👇 Click to hide notifications -->
+              <button phx-click="toggle_notification" class="text-right w-full text-xs text-gray-500">Hide Notifications</button>
+              
+              <%= if @notification_visible do %>
+                <%= for {user, message, time_passed, timestamp} <- @notifs do %>
+                  <div class="flex justify-between align-center items-center mb-5">
+                    <div class="flex justify-center items-center">
+                      <img
+                        class="h-11 w-11 rounded-full"
+                        src={user.avatar_url || "/images/default-user.svg"}
+                      />
+                      <div class="ml-3.5 text-sm leading-tight mt-5">
+                        <span class="block text-[#60616D] text-sm">
+                          <a
+                            class="text-blue-500"
+                            href={
+                              Routes.live_path(
+                                @socket,
+                                MazarynWeb.UserLive.Profile,
+                                user.username,
+                                @locale
+                              )
+                            }
+                          >
+                            <%= user.username %>
+                          </a>
+                        </span>
+                        <span class="block text-[#60616D] text-sm"><%= message %></span>
+                        <span class="block text-[#60616D] text-sm"><%= time_passed %></span>
+                      </div>
                     </div>
                   </div>
-                </div>
+                <% end %>
               <% end %>
             </div>
           </div>
@@ -90,20 +96,25 @@ defmodule MazarynWeb.HomeLive.Notification do
   def handle_info(:time_diff, socket) do
     notifs =
       socket.assigns.notifs
-      |> Enum.map(fn {user, message, _time_passed, time_stamp} ->
+      |> Enum.map(fn {user, message, _old_time_passed, time_stamp} ->
         time_passed = time_passed(time_stamp)
         {user, message, time_passed, time_stamp}
       end)
 
     Process.send_after(self(), :time_diff, 1000)
-
     {:noreply, assign(socket, :notifs, notifs)}
+  end
+
+  # 👇 Handle toggle visibility event
+  @impl true
+  def handle_event("toggle_notification", _params, socket) do
+    {:noreply, assign(socket, :notification_visible, false)}
   end
 
   defp get_all_user_notifs(user) do
     user.id
     |> Core.NotifEvent.get_all_notifs()
-    |> Enum.map(fn {:notif, _notif_id, actor_id, target_id, message, time_stamp, _metadata} ->
+    |> Enum.map(fn {:notif, notifid, actor_id, target_id, message, time_stamp, _metadata} ->
       {:ok, user} = get_user(actor_id, target_id)
       time_passed = time_passed(time_stamp)
       {user, message, time_passed, time_stamp}
@@ -112,7 +123,6 @@ defmodule MazarynWeb.HomeLive.Notification do
 
   defp time_passed(time_stamp) do
     date_time = Timex.to_datetime(time_stamp)
-
     time_difference([:years, :months, :weeks, :days, :hours, :minutes, :seconds], date_time)
   end
 
@@ -123,7 +133,7 @@ defmodule MazarynWeb.HomeLive.Notification do
     end
   end
 
-  defp time_difference([h | t] = _granulaties, date_time) do
+  defp time_difference([h | t], date_time) do
     case Timex.diff(Timex.now(), date_time, h) do
       0 -> time_difference(t, date_time)
       diff -> "#{diff} #{h} ago"
@@ -134,9 +144,10 @@ defmodule MazarynWeb.HomeLive.Notification do
     id =
       case actor_id do
         :undefined -> target_id
-        actor_id -> actor_id
+        _ -> actor_id
       end
 
     Users.one_by_id(id)
   end
 end
+
