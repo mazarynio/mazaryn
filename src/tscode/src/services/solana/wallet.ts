7c6c5569185405491b752dfce9fd98382aa4afc6
Tim <neshtimo38@gmail.com>
2026-02-13 20:21:09 +0300

    add keypair signer

--- src/tscode/src/services/solana/wallet.ts
+++ src/tscode/src/services/solana/wallet.ts
@@ -1,1 +1,15 @@
+import { generateKeyPair, generateKeyPairSigner } from '@solana/kit';
+
+
+export class AccountManager {
+  async createKeyPairSigner() {
+    try {
+      const wallet = await generateKeyPairSigner()
+    } catch (error) {
+      console.log("Something went wrong", error);
+    }
+  }
+}
+
+
 console.log("Not implemented yet...!");
